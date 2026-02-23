9047f79c5868eaeec2ed02bef707e1a904b7756e
DininduR20 <130210880+DininduR20@users.noreply.github.com>
2026-02-23 20:52:22 +0530

    Task 2

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+
 console.log('Hello World!');
+// task 1
 const fs = require('fs');
 fs.readFile('file.txt', 'utf8', function (err, data) {
  if (err) throw err;
  console.log(data);
+});
+
+//task 2
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
 });
